d2a34556b1fe662c71680e4c7fc00544d53ce11d
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2025-01-07 19:40:30 +0100

    feat(tiptap-editor): style editor and add extensions
    - Add custom CSS for tiptap editor
    - Implement extensions: TaskItem, TaskList, Table, Image
    - Update editor configuration to support new extensions

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,18 @@
+import Editor from "./Editor"
+
+interface DocumentIdPageProps {
+    params: Promise<{ documentId: string }>
+}
+
+const DocumentPage = async ({ params }: DocumentIdPageProps) => {
+    const { documentId } = await params
+
+    return (
+        <div className="min-h-screen bg[#FAFBFD]">
+            <Editor />
+        </div>
+
+    )
+}
+
+export default DocumentPage
