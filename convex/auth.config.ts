d369d0f5c4582fc7119466365b051a24c6fb105e
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2025-01-16 14:33:54 +0100

    feat(clerk-setup): add Clerk authentication and configurations

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://feasible-hagfish-9.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react"
 import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { FullScreenLoader } from "./full-screen-loader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+      <ConvexProviderWithClerk
+        client={convex}
+        useAuth={useAuth}
+      >
+        <Authenticated>
+          {children}
+        </Authenticated>
+        <Unauthenticated>
+          <div className="flex flex-col justify-center items-center min-h-screen">
+            <SignIn />
+          </div>
+        </Unauthenticated>
+        <AuthLoading>
+          <FullScreenLoader />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 };
