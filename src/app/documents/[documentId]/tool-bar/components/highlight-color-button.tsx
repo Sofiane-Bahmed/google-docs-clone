a775393161c371fa99954994aa821431c33368d7
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2025-01-29 23:38:13 +0100

    add toolbar/components file

---
+++ src/app/documents/[documentId]/tool-bar/components/highlight-color-button.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import {
+    type ColorResult,
+    SketchPicker
+} from "react-color"
+
+import { useEditorStore } from '@/store/use-editor-store'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { HighlighterIcon } from "lucide-react";
+
+export const HighlightColorButton = () => {
+    const { editor } = useEditorStore()
+
+    const value = editor?.getAttributes("highlight").color || "#FFFFFF"
+
+    const onChange = (color: ColorResult) => {
+        editor?.chain().focus().setHighlight({ color: color.hex }).run()
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-[#d3e3fd] px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker
+                    color={value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
